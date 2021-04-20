b2095120d6aa3d7ad71a48a095da0ec0e5a93775
Tiv0w <t.theomeyer@gmail.com>
2021-04-20 09:21:19 +0200

    :art: Use bitwise operations to be clearer

--- src/utils.rs
+++ src/utils.rs
@@ -2,8 +2,9 @@ pub fn get_hex_digits(opcode: u16) -> [u8; 4] {
     let mut digits_array: [u8; 4] = [0; 4];
 
     for i in 0..4 {
-        let hex: u16 = 0x10;
-        digits_array[i] = ((opcode / hex.pow(i as u32)) % hex) as u8;
+        // Shifts by 4 and takes only last 4
+        let hex_digit = (opcode >> i * 4) & 0x0F;
+        digits_array[i] = hex_digit as u8;
     }
     digits_array.reverse();
 
